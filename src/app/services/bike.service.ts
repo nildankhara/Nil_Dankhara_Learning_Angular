aed843db3cbee3b5bf8e97493c70955e43bbd930
“Nil <w0859772@myscc.ca>
2024-10-09 20:09:20 -0400

    Step 8 and 9 done

--- src/app/services/bike.service.ts
+++ src/app/services/bike.service.ts
@@ -7,10 +7,32 @@ import {bikeList} from "../data/mock-bike";
   providedIn: 'root'
 })
 export class BikeService {
+  private bikes: Bike[] = bikeList;
 
   constructor() {
   }
   getBikes(): Observable<Bike[]> {
     return of(bikeList);
+  }
+  addBike(newBike: Bike): Observable<Bike[]> {
+    this.bikes.push(newBike);
+    return of(this.bikes);
+  }
+
+  updateBike(updatedBike: Bike): Observable<Bike[]> {
+    const index = this.bikes.findIndex(bike => bike.number === updatedBike.number);
+    if (index !== -1) {
+      this.bikes[index] = updatedBike;
+    }
+    return of(this.bikes);
+  }
+
+  deleteBike(bikeNumber: number): Observable<Bike[]> {
+    this.bikes = this.bikes.filter(bike => bikeNumber !== bikeNumber);
+    return of(this.bikes);
+  }
+  getBikesByNumber(bikeNumber: number): Observable<Bike[]> {
+    const bike = this.bikes.find(bike => bikeNumber !== bikeNumber);
+    return of(this.bikes);
   }
 }
